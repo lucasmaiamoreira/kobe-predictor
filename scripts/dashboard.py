a6e41fb31dfc53db99ef556a9f128f074ae55273
lucasmaiamoreira.com@gmail.com <lucasmaiamoreira.com@gmail.com>
2024-04-08 23:01:34 -0300

    Correção códigos em geral

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -1,46 +1,144 @@
+# import streamlit as st
+# import pandas as pd
+# import mlflow
+
+# # Função para carregar os dados de produção e as previsões
+# def load_data_and_predictions():
+#     # Carregar os dados de produção
+#     data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
+#     # Carregar as previsões
+#     predictions = pd.read_parquet("output/predictions.parquet")
+#     return data_prod, predictions
+
+# # Função para carregar as métricas do modelo
+# def load_model_metrics():
+#     with mlflow.start_run(run_id="f253d7ed10e549199300788109fea8a2") as run:
+#         # Utilize mlflow.log_metric para registrar as métricas, não mlflow.get_metric
+#         log_loss_prod = mlflow.log_metric("log_loss_prod")
+#         f1_score_prod = mlflow.log_metric("f1_score_prod")
+#     return log_loss_prod, f1_score_prod
+
+# # Função principal do dashboard
+# def main():
+#     # Título do dashboard
+#     st.title("Monitoramento do Modelo de Classificação de Kobe")
+
+#     # Carregar os dados e as previsões
+#     data_prod, predictions = load_data_and_predictions()
+
+#     # Carregar as métricas do modelo
+#     log_loss_prod, f1_score_prod = load_model_metrics()
+
+#     # Exibir os dados de produção
+#     st.subheader("Dados de Produção:")
+#     st.write(data_prod)
+
+#     # Exibir as previsões
+#     st.subheader("Previsões:")
+#     st.write(predictions)
+
+#     # Exibir as métricas do modelo
+#     st.subheader("Métricas do Modelo:")
+#     st.write(f"Log Loss: {log_loss_prod}")
+#     st.write(f"F1 Score: {f1_score_prod}")
+
+# if __name__ == "__main__":
+#     main()
+
+# import streamlit as st
+# import mlflow.pyfunc
+
+# mlflow.set_tracking_uri("http://localhost:5000")
+
+# # Carregar o modelo MLflow
+# model_uri = "mlflow-artifacts:/0/a4156b87f8f84ac6839cde1e44e78fd4/artifacts/final_model"
+# loaded_model = mlflow.pyfunc.load_model(model_uri)
+
+# # Função para fazer previsões com o modelo
+# def predict(input_data):
+#     return loaded_model.predict(input_data)
+
+# # Função principal do aplicativo Streamlit
+# def main():
+#     # Interface do usuário para entrada de dados
+#     st.title("Previsão com Modelo MLflow")
+#     input_data = st.text_input("Insira os dados de entrada separados por vírgula")
+
+#     if st.button("Prever"):
+#         # Converter entrada de texto em uma lista de valores
+#         input_data = list(map(float, input_data.split(',')))
+
+#         # Fazer previsões com o modelo
+#         prediction = predict([input_data])
+
+#         # Exibir resultado da previsão
+#         st.write("Resultado da Previsão:", prediction)
+
+# if __name__ == "__main__":
+#     main()
+
+
 import streamlit as st
+import mlflow.pyfunc
 import pandas as pd
-import mlflow
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Função para carregar os dados de produção e as previsões
-def load_data_and_predictions():
-    # Carregar os dados de produção
-    data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
-    # Carregar as previsões
-    predictions = pd.read_parquet("output/predictions.parquet")
-    return data_prod, predictions
+mlflow.set_tracking_uri("http://localhost:5000")
 
-# Função para carregar as métricas do modelo
-def load_model_metrics():
-    with mlflow.start_run(run_id="f253d7ed10e549199300788109fea8a2") as run:
-        # Utilize mlflow.log_metric para registrar as métricas, não mlflow.get_metric
-        log_loss_prod = mlflow.log_metric("log_loss_prod")
-        f1_score_prod = mlflow.log_metric("f1_score_prod")
-    return log_loss_prod, f1_score_prod
+# Carregar o modelo MLflow
+model_uri = "mlflow-artifacts:/0/a4156b87f8f84ac6839cde1e44e78fd4/artifacts/final_model"
+loaded_model = mlflow.pyfunc.load_model(model_uri)
 
-# Função principal do dashboard
+# Função para fazer previsões com o modelo
+def predict(input_data):
+    return loaded_model.predict(input_data)
+
+# Função principal do aplicativo Streamlit
 def main():
-    # Título do dashboard
-    st.title("Monitoramento do Modelo de Classificação de Kobe")
+    # Interface do usuário para entrada de dados
+    st.title("Previsão com Modelo MLflow")
 
-    # Carregar os dados e as previsões
-    data_prod, predictions = load_data_and_predictions()
+    st.sidebar.title("Previsão de Arremessos")
 
-    # Carregar as métricas do modelo
-    log_loss_prod, f1_score_prod = load_model_metrics()
+    # Adicione sliders para entrada de dados
+    lat = st.sidebar.slider("Latitude", min_value=-90.0, max_value=90.0, step=0.1, value=0.0)
+    lon = st.sidebar.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.1, value=0.0)
+    minutes_remaining = st.sidebar.slider("Minutos Restantes", min_value=0, max_value=48, step=1, value=0)
+    period = st.sidebar.slider("Período", min_value=1, max_value=4, step=1, value=1)
+    playoffs = st.sidebar.radio("Playoffs", options=["Sim", "Não"], index=0)
+    playoffs = 1 if playoffs == "Sim" else 0
+    shot_distance = st.sidebar.slider("Distância do Arremesso", min_value=0, max_value=50, step=1, value=0)
 
-    # Exibir os dados de produção
-    st.subheader("Dados de Produção:")
-    st.write(data_prod)
 
-    # Exibir as previsões
-    st.subheader("Previsões:")
-    st.write(predictions)
+    if st.sidebar.button("Prever"):
+        # Organize os dados de entrada em uma lista
+        input_data = [lat, lon, minutes_remaining, period, playoffs, shot_distance]
 
-    # Exibir as métricas do modelo
-    st.subheader("Métricas do Modelo:")
-    st.write(f"Log Loss: {log_loss_prod}")
-    st.write(f"F1 Score: {f1_score_prod}")
+        # Fazer previsões com o modelo
+        prediction = predict([input_data])
+
+        # Exibir resultado da previsão
+        st.write("Resultado da Previsão:", prediction)
+
+        # Visualização dos dados de entrada
+        input_df = pd.DataFrame({'Latitude': [lat], 'Longitude': [lon], 'Minutos Restantes': [minutes_remaining],
+                                 'Período': [period], 'Playoffs': [playoffs], 'Distância do Arremesso': [shot_distance]})
+        st.write("Dados de Entrada:")
+        st.write(input_df)
+
+        # Histograma das previsões
+        st.write("Histograma das Previsões:")
+        fig, ax = plt.subplots()
+        ax.hist(prediction)
+        st.pyplot(fig)
+
+        # Mapa de calor das correlações entre as características
+        st.write("Mapa de Calor das Correlações:")
+        fig, ax = plt.subplots()
+        sns.heatmap(input_df.corr(), annot=True, ax=ax)
+        st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
+
--- scripts/preparacao_dados.py
+++ scripts/preparacao_dados.py
@@ -1,43 +1,284 @@
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# import mlflow
+# import mlflow.sklearn
+
+# # Iniciar uma nova run do MLflow com o nome "PreparacaoDados"
+# mlflow.start_run(run_name="PreparacaoDados")
+
+# # Carregar os dados de treinamento e produção
+# data_dev = pd.read_parquet("data/dataset_kobe_dev.parquet")
+# data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
+
+# # Selecionar as colunas relevantes
+# relevant_columns = ['lat', 'lon', 'minutes_remaining', 'period', 'playoffs', 'shot_distance', 'shot_made_flag']
+# data_dev_filtered = data_dev[relevant_columns].dropna()
+# data_prod_filtered = data_prod[relevant_columns[:-1]].dropna()  # Excluir a coluna 'shot_made_flag' dos dados de produção
+
+# # Salvar o dataset processado
+# data_dev_filtered.to_parquet("data/processed/data_filtered_dev.parquet")
+# data_prod_filtered.to_parquet("data/processed/data_filtered_prod.parquet")
+
+# # Separar os dados em treino e teste (80% treino, 20% teste)
+# X_train, X_test, y_train, y_test = train_test_split(data_dev_filtered.drop(columns=['shot_made_flag']), 
+#                                                     data_dev_filtered['shot_made_flag'], 
+#                                                     test_size=0.2, 
+#                                                     stratify=data_dev_filtered['shot_made_flag'], 
+#                                                     random_state=42)
+
+# # Incluir a coluna 'shot_made_flag' nos datasets de treino e teste
+# train_data = pd.concat([X_train, y_train], axis=1)
+# test_data = pd.concat([X_test, y_test], axis=1)
+
+# # Salvar os datasets de treino e teste em arquivos Parquet
+# train_data.to_parquet("data/processed/base_train.parquet")
+# test_data.to_parquet("data/processed/base_test.parquet")
+
+# # Registrar os parâmetros e métricas no MLflow
+# mlflow.log_param("percent_test", 0.2)
+# mlflow.log_metric("train_size", len(X_train))
+# mlflow.log_metric("test_size", len(X_test))
+
+# # Finalizar a run do MLflow
+# mlflow.end_run()
+
+
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# import mlflow
+# import mlflow.sklearn
+# from sklearn.ensemble import RandomForestClassifier
+
+# mlflow.set_tracking_uri("http://localhost:5000")
+
+# # Iniciar uma nova run do MLflow com o nome "PreparacaoDados"
+# mlflow.start_run(run_name="PreparacaoDados")
+
+# # Carregar os dados de treinamento e produção
+# data_dev = pd.read_parquet("data/dataset_kobe_dev.parquet")
+# data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
+
+# # Selecionar as colunas relevantes
+# relevant_columns = ['lat', 'lon', 'minutes_remaining', 'period', 'playoffs', 'shot_distance', 'shot_made_flag']
+# data_dev_filtered = data_dev[relevant_columns].dropna()
+# data_prod_filtered = data_prod[relevant_columns[:-1]].dropna()  # Excluir a coluna 'shot_made_flag' dos dados de produção
+
+# # Salvar o dataset processado
+# data_dev_filtered.to_parquet("data/processed/data_filtered.parquet")
+
+# # Separar os dados em treino e teste (80% treino, 20% teste)
+# X = data_dev_filtered.drop(columns=['shot_made_flag'])
+# y = data_dev_filtered['shot_made_flag']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+# # Incluir a coluna 'shot_made_flag' nos datasets de treino e teste
+# train_data = pd.concat([X_train, y_train], axis=1)
+# test_data = pd.concat([X_test, y_test], axis=1)
+
+# # Salvar os datasets de treino e teste em arquivos Parquet
+# train_data.to_parquet("data/processed/base_train.parquet")
+# test_data.to_parquet("data/processed/base_test.parquet")
+
+# # Registrar os parâmetros e métricas no MLflow
+# mlflow.log_param("percent_test", 0.2)
+# mlflow.log_metric("train_size", len(X_train))
+# mlflow.log_metric("test_size", len(X_test))
+
+# # Registrar uma explicação sobre a escolha de treino e teste
+# explanation = """
+# A escolha de treino e teste é essencial para avaliar o desempenho do modelo de machine learning. 
+# Usamos uma escolha aleatória e estratificada para garantir que as proporções de classes sejam mantidas nos conjuntos de treino e teste. 
+# Isso ajuda a evitar viés nos dados e a garantir que o modelo seja avaliado de forma justa.
+# Para minimizar os efeitos de viés de dados, podemos aplicar técnicas como validação cruzada, uso de dados de validação, balanceamento de classes e coleta de mais dados quando possível.
+# """
+# mlflow.log_param("explanation", explanation)
+
+# # Treinar seu modelo (substitua RandomForestClassifier pelo modelo que você está usando)
+# sk_model = RandomForestClassifier()
+# sk_model.fit(X_train, y_train)
+
+# # Registrar o modelo no MLflow
+# mlflow.sklearn.log_model(
+#     sk_model, 
+#     "PreparacaoDados",
+#     registered_model_name="PreparacaoDados"
+#     )
+
+# # Finalizar a run do MLflow
+# mlflow.end_run()
+
+
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# import mlflow
+# import mlflow.sklearn
+# from sklearn.ensemble import RandomForestClassifier
+# import tempfile
+# import os
+
+# mlflow.set_tracking_uri("http://localhost:5000")
+
+# # Iniciar uma nova run do MLflow com o nome "PreparacaoDados"
+# with mlflow.start_run(run_name="PreparacaoDados"):
+
+#     # Carregar os dados de treinamento e produção
+#     data_dev = pd.read_parquet("data/dataset_kobe_dev.parquet")
+#     data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
+
+#     # Selecionar as colunas relevantes
+#     relevant_columns = ['lat', 'lon', 'minutes_remaining', 'period', 'playoffs', 'shot_distance', 'shot_made_flag']
+#     data_dev_filtered = data_dev[relevant_columns].dropna()
+#     data_prod_filtered = data_prod[relevant_columns[:-1]].dropna()  # Excluir a coluna 'shot_made_flag' dos dados de produção
+
+#     # Salvar o dataset processado
+#     data_dev_filtered.to_parquet("data/processed/data_filtered.parquet")
+
+#     # Separar os dados em treino e teste (80% treino, 20% teste)
+#     X = data_dev_filtered.drop(columns=['shot_made_flag'])
+#     y = data_dev_filtered['shot_made_flag']
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+#     # Incluir a coluna 'shot_made_flag' nos datasets de treino e teste
+#     train_data = pd.concat([X_train, y_train], axis=1)
+#     test_data = pd.concat([X_test, y_test], axis=1)
+
+#     # Salvar os datasets de treino e teste em arquivos Parquet
+#     train_data.to_parquet("data/processed/base_train.parquet")
+#     test_data.to_parquet("data/processed/base_test.parquet")
+
+#     # Calcular o percentual de teste
+#     percent_test = len(test_data) / (len(train_data) + len(test_data))
+
+#     # Registrar os parâmetros e métricas no MLflow
+#     mlflow.log_param("percent_test", percent_test)
+#     mlflow.log_metric("train_size", len(train_data))
+#     mlflow.log_metric("test_size", len(test_data))
+
+#     # Registrar uma explicação sobre a escolha de treino e teste
+#     explanation = """
+#     A escolha de treino e teste é essencial para avaliar o desempenho do modelo de machine learning. 
+#     Usamos uma escolha aleatória e estratificada para garantir que as proporções de classes sejam mantidas nos conjuntos de treino e teste. 
+#     Isso ajuda a evitar viés nos dados e a garantir que o modelo seja avaliado de forma justa.
+#     Para minimizar os efeitos de viés de dados, podemos aplicar técnicas como validação cruzada, uso de dados de validação, balanceamento de classes e coleta de mais dados quando possível.
+#     """
+    
+#     # Criar um arquivo temporário para salvar a explicação
+#     with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+#         tmp_file.write(explanation)
+
+#     # Registrar o arquivo temporário como um artefato no MLflow
+#     mlflow.log_artifact(tmp_file.name, "explanation.txt")
+
+#     # Remover o arquivo temporário
+#     os.unlink(tmp_file.name)
+
+#     # Treinar seu modelo (substitua RandomForestClassifier pelo modelo que você está usando)
+#     sk_model = RandomForestClassifier()
+#     sk_model.fit(X_train, y_train)
+
+#     # Registrar o modelo no MLflow
+#     mlflow.sklearn.log_model(
+#         sk_model, 
+#         "PreparacaoDados",
+#         registered_model_name="PreparacaoDados"
+#     )
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tempfile
+import os
+
+mlflow.set_tracking_uri("http://localhost:5000")
 
 # Iniciar uma nova run do MLflow com o nome "PreparacaoDados"
-mlflow.start_run(run_name="PreparacaoDados")
+with mlflow.start_run(run_name="PreparacaoDados"):
 
-# Carregar os dados de treinamento e produção
-data_dev = pd.read_parquet("data/dataset_kobe_dev.parquet")
-data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
+    # Carregar os dados de treinamento e produção
+    data_dev = pd.read_parquet("data/dataset_kobe_dev.parquet")
+    data_prod = pd.read_parquet("data/dataset_kobe_prod.parquet")
 
-# Selecionar as colunas relevantes
-relevant_columns = ['lat', 'lon', 'minutes_remaining', 'period', 'playoffs', 'shot_distance', 'shot_made_flag']
-data_dev_filtered = data_dev[relevant_columns].dropna()
-data_prod_filtered = data_prod[relevant_columns[:-1]].dropna()  # Excluir a coluna 'shot_made_flag' dos dados de produção
+    # Selecionar as colunas relevantes
+    relevant_columns = ['lat', 'lon', 'minutes_remaining', 'period', 'playoffs', 'shot_distance', 'shot_made_flag']
+    data_dev_filtered = data_dev[relevant_columns].dropna()
+    data_prod_filtered = data_prod[relevant_columns[:-1]].dropna()  # Excluir a coluna 'shot_made_flag' dos dados de produção
 
-# Salvar o dataset processado
-data_dev_filtered.to_parquet("data/processed/data_filtered_dev.parquet")
-data_prod_filtered.to_parquet("data/processed/data_filtered_prod.parquet")
+    # Salvar o dataset processado
+    data_dev_filtered.to_parquet("data/processed/data_filtered.parquet")
 
-# Separar os dados em treino e teste (80% treino, 20% teste)
-X_train, X_test, y_train, y_test = train_test_split(data_dev_filtered.drop(columns=['shot_made_flag']), 
-                                                    data_dev_filtered['shot_made_flag'], 
-                                                    test_size=0.2, 
-                                                    stratify=data_dev_filtered['shot_made_flag'], 
-                                                    random_state=42)
+    # Separar os dados em treino e teste (80% treino, 20% teste)
+    X = data_dev_filtered.drop(columns=['shot_made_flag'])
+    y = data_dev_filtered['shot_made_flag']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-# Incluir a coluna 'shot_made_flag' nos datasets de treino e teste
-train_data = pd.concat([X_train, y_train], axis=1)
-test_data = pd.concat([X_test, y_test], axis=1)
+    # Incluir a coluna 'shot_made_flag' nos datasets de treino e teste
+    train_data = pd.concat([X_train, y_train], axis=1)
+    test_data = pd.concat([X_test, y_test], axis=1)
 
-# Salvar os datasets de treino e teste em arquivos Parquet
-train_data.to_parquet("data/processed/base_train.parquet")
-test_data.to_parquet("data/processed/base_test.parquet")
+    # Salvar os datasets de treino e teste em arquivos Parquet
+    train_data.to_parquet("data/processed/base_train.parquet")
+    test_data.to_parquet("data/processed/base_test.parquet")
 
-# Registrar os parâmetros e métricas no MLflow
-mlflow.log_param("percent_test", 0.2)
-mlflow.log_metric("train_size", len(X_train))
-mlflow.log_metric("test_size", len(X_test))
+    # Calcular o percentual de teste
+    percent_test = len(test_data) / (len(train_data) + len(test_data))
 
-# Finalizar a run do MLflow
-mlflow.end_run()
+    # Registrar os parâmetros e métricas no MLflow
+    mlflow.log_param("percent_test", percent_test)
+    mlflow.log_metric("train_size", len(train_data))
+    mlflow.log_metric("test_size", len(test_data))
+
+    # Registrar uma explicação sobre a escolha de treino e teste
+    explanation = """
+    A escolha de treino e teste é essencial para avaliar o desempenho do modelo de machine learning. 
+    Usamos uma escolha aleatória e estratificada para garantir que as proporções de classes sejam mantidas nos conjuntos de treino e teste. 
+    Isso ajuda a evitar viés nos dados e a garantir que o modelo seja avaliado de forma justa.
+    Para minimizar os efeitos de viés de dados, podemos aplicar técnicas como validação cruzada, uso de dados de validação, balanceamento de classes e coleta de mais dados quando possível.
+    """
+    
+    # Criar um arquivo temporário para salvar a explicação
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        tmp_file.write(explanation)
+
+    # Registrar o arquivo temporário como um artefato no MLflow
+    mlflow.log_artifact(tmp_file.name, "explanation.txt")
+
+    # Remover o arquivo temporário
+    os.unlink(tmp_file.name)
+
+    # Treinar seu modelo (substitua RandomForestClassifier pelo modelo que você está usando)
+    sk_model = RandomForestClassifier()
+    sk_model.fit(X_train, y_train)
+
+    # Registrar o modelo no MLflow
+    mlflow.sklearn.log_model(
+        sk_model, 
+        "PreparacaoDados",
+        registered_model_name="PreparacaoDados",
+        
+    )
+
+    # Plot histograms for numeric variables
+    for column in ['lat', 'lon', 'minutes_remaining', 'period', 'shot_distance']:
+        plt.figure()
+        sns.histplot(train_data[column], kde=True)
+        plt.title(f'Histogram for {column}')
+        plt.xlabel(column)
+        plt.ylabel('Frequency')
+        plt.savefig(f"output/histogram_{column}.png")
+        plt.close()
+        mlflow.log_artifact(f"output/histogram_{column}.png")
+
+    # Plot bar plots for categorical variables
+    for column in ['playoffs', 'shot_made_flag']:
+        plt.figure()
+        sns.countplot(x=column, data=train_data)
+        plt.title(f'Countplot for {column}')
+        plt.xlabel(column)
+        plt.ylabel('Count')
+        plt.savefig(f"output/countplot_{column}.png")
+        plt.close()
+        mlflow.log_artifact(f"output/countplot_{column}.png")
